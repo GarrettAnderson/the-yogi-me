2ab9d1f244eefe90c7b7618e5d4f7f135a752570
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-20 23:34:20 -0400

    created basic html in the hello world component

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,7 +2,148 @@ import React, { Component } from 'react'
 
 class HelloWorld extends Component {
   render() {
-    return <h1>Hello, World!</h1>
+    return (
+      <section>
+        {/* SplashPage HTML */}
+        <header>
+          <h1>The Yogi Me</h1>
+        </header>
+        <main>
+          <section>
+            <h3>Fun Facts</h3>
+          </section>
+          <section>
+            <h3>Pose Directory</h3>
+          </section>
+          <section>
+            <h3>Your Classes</h3>
+          </section>
+        </main>
+        {/* SplashPage end */}
+
+        {/* Fun Facts HTML */}
+        <header>
+          <nav>The Yogi Me</nav>
+        </header>
+        <main>
+          <ol>
+            <li>
+              <figure>
+                <header>Date</header>
+                <figcaption>
+                  Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird of paradise pose
+                  crab pose staff pose flying crow pose sage koundinya i pose one-legged king pigeon pose ii eagle pose
+                  cow face pose marichi's pose ii big toe pose gate pose full boat pose marichi's pose iii, seated twist
+                  side crane pose, side crow pose yoga plank pose shoulderstand, supported shoulderstand locust pose
+                  easy pose knees to chest pose upward facing dog pose chair pose warrior ii pose hero pose.
+                </figcaption>
+              </figure>
+            </li>
+            <li>
+              <figure>
+                <header>Date</header>
+                <figcaption>
+                  Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird of paradise pose
+                  crab pose staff pose flying crow pose sage koundinya i pose one-legged king pigeon pose ii eagle pose
+                  cow face pose marichi's pose ii big toe pose gate pose full boat pose marichi's pose iii, seated twist
+                  side crane pose, side crow pose yoga plank pose shoulderstand, supported shoulderstand locust pose
+                  easy pose knees to chest pose upward facing dog pose chair pose warrior ii pose hero pose.
+                </figcaption>
+              </figure>
+            </li>
+            <li>
+              <figure>
+                <header>Date</header>
+                <figcaption>
+                  Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird of paradise pose
+                  crab pose staff pose flying crow pose sage koundinya i pose one-legged king pigeon pose ii eagle pose
+                  cow face pose marichi's pose ii big toe pose gate pose full boat pose marichi's pose iii, seated twist
+                  side crane pose, side crow pose yoga plank pose shoulderstand, supported shoulderstand locust pose
+                  easy pose knees to chest pose upward facing dog pose chair pose warrior ii pose hero pose.
+                </figcaption>
+              </figure>
+            </li>
+            <li>
+              <figure>
+                <header>Date</header>
+                <figcaption>
+                  Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird of paradise pose
+                  crab pose staff pose flying crow pose sage koundinya i pose one-legged king pigeon pose ii eagle pose
+                  cow face pose marichi's pose ii big toe pose gate pose full boat pose marichi's pose iii, seated twist
+                  side crane pose, side crow pose yoga plank pose shoulderstand, supported shoulderstand locust pose
+                  easy pose knees to chest pose upward facing dog pose chair pose warrior ii pose hero pose.
+                </figcaption>
+              </figure>
+            </li>
+          </ol>
+        </main>
+        {/* Fun Facts end */}
+
+        {/* Yoga Poses */}
+        <header>
+          <nav>The Yogi Me</nav>
+        </header>
+        <main>
+          <ol>
+            <li>
+              <img src="" />
+              <p>Yoga Pose Name</p>
+            </li>
+            <li>
+              <img src="" />
+              <p>Yoga Pose Name</p>
+            </li>
+            <li>
+              <img src="" />
+              <p>Yoga Pose Name</p>
+            </li>
+            <li>
+              <img src="" />
+              <p>Yoga Pose Name</p>
+            </li>
+            <li>
+              <img src="" />
+              <p>Yoga Pose Name</p>
+            </li>
+            <li>
+              <img src="" />
+              <p>Yoga Pose Name</p>
+            </li>
+          </ol>
+        </main>
+        {/* Yoga Poses end*/}
+
+        {/* Yoga Pose Detail */}
+        <header>
+          <nav>The Yogi Me</nav>
+        </header>
+        <main>
+          <figure>
+            <img src="" />
+            <ol>
+              <li>
+                <h2>Sanskrit Name</h2>
+              </li>
+              <li>
+                <h3>English Name</h3>
+              </li>
+              <li>Position Type</li>
+            </ol>
+            <h2>Steps to Get into the Pose</h2>
+            <li>Step 1</li>
+            <li>Step 2</li>
+            <li>Step 3</li>
+            <li>Step 4</li>
+            <li>Step 5</li>
+            <figcaption>
+              <h3>Description</h3>Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird
+              of paradise pose crab pose staff pose flying crow pose.
+            </figcaption>
+          </figure>
+        </main>
+        {/* Yoga Pose Detail end */}
+      </section>
+    )
   }
 }
 
