36ebe1538af437cc66612bf89966e8f364db128c
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-04-03 02:20:28 -0400

    added more styles to center the content on the yoga pose details sceen. updated html in the component to break up the steps list

--- src/pages/YogaPose.js
+++ src/pages/YogaPose.js
@@ -4,30 +4,28 @@ import { Link } from 'react-router-dom'
 class YogaPose extends Component {
   render() {
     return (
-      <section>
+      <section className="single-pose-container">
         <header className="header-nav">
           <Link to="/">
             <h1>The Yogi Me</h1>
           </Link>
         </header>
-        <main>
+        <main className="single-pose-details">
           <figure>
             <img src={require('../images/yoga_stock_img.jpg')} className="pose-detail-img" alt="tree-pose-image" />
-            <ol>
-              <li>
-                <h2>Sanskrit Name</h2>
-              </li>
-              <li>
-                <h3>English Name</h3>
-              </li>
+            <ol className="pose-details-list">
+              <li>Sanskrit Name</li>
+              <li>English Name</li>
               <li>Position Type</li>
             </ol>
-            <h2>Steps to Get into the Pose</h2>
-            <li>Step 1</li>
-            <li>Step 2</li>
-            <li>Step 3</li>
-            <li>Step 4</li>
-            <li>Step 5</li>
+            <ol>
+              <h5>Steps to Get into the Pose</h5>
+              <li>Step 1</li>
+              <li>Step 2</li>
+              <li>Step 3</li>
+              <li>Step 4</li>
+              <li>Step 5</li>
+            </ol>
             <figcaption>
               <h3>Description</h3>Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird
               of paradise pose crab pose staff pose flying crow pose.
