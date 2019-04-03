de9f1bb381c5324e75b7c9a2e35c12f876debb8e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-04-03 08:34:51 -0400

    more styles to the detail card. added bottstrap list-group classes and gave the ol elements whidth of 100percent to better organized the content

--- src/pages/YogaPose.js
+++ src/pages/YogaPose.js
@@ -12,20 +12,20 @@ class YogaPose extends Component {
         </header>
         <main className="single-pose-details detail-card-outline">
           <section>
-            <ol className="pose-details-list">
-              <li>Sanskrit Name</li>
-              <li>English Name</li>
-              <li>Position Type</li>
+            <ol className="pose-details-list list-group">
+              <li className="list-group-item">Sanskrit Name</li>
+              <li className="list-group-item">English Name</li>
+              <li className="list-group-item">Position Type</li>
             </ol>
 
-            <h6>Steps to Get into the Pose</h6>
-            <ol>
-              <li>Step 1</li>
-              <li>Step 2</li>
-              <li>Step 3</li>
-              <li>Step 4</li>
-              <li>Step 5</li>
-              <li>
+            <ol className="pose-details-list list-group">
+              <header>Steps to Get into the Pose</header>
+              <li className="list-group-item">Step 1</li>
+              <li className="list-group-item"> Step 2</li>
+              <li className="list-group-item">Step 3</li>
+              <li className="list-group-item">Step 4</li>
+              <li className="list-group-item">Step 5</li>
+              <li className="list-group-item">
                 <b>Description:</b> Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird
                 of paradise pose crab pose staff pose flying crow pose.
               </li>
