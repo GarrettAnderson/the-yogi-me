aba73b756d6c6b1d7a955d2863b4f2915d547387
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-04-02 22:13:16 -0400

    added some styles to the hompage. used bootstrap to make the ui look more organized. added css to change the font color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,19 +12,25 @@ class Home extends Component {
         </header>
         <main className="home-page-container">
           <section>
-            <Link to="/YogaHistory">
-              <h3>Fun Facts</h3>
-            </Link>
-          </section>
-          <section>
-            <Link to="/YogaPoses">
-              <h3>Pose Directory</h3>
-            </Link>
-          </section>
-          <section>
-            <Link to="/YogaSequenceBuilder">
-              <h3>Your Classes</h3>
-            </Link>
+            <ul className="list-group">
+              <Link to="/YogaHistory">
+                <li className="list-group-item">
+                  <h3>Fun Facts</h3>
+                </li>
+              </Link>
+
+              <Link to="/YogaPoses">
+                <li className="list-group-item">
+                  <h3>Pose Directory</h3>
+                </li>
+              </Link>
+
+              <Link to="/YogaSequenceBuilder">
+                <li className="list-group-item">
+                  <h3>Your Classes</h3>
+                </li>
+              </Link>
+            </ul>
           </section>
         </main>
       </section>
