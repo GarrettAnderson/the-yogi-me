630b350edfe99f831adfc6ace40aaea85d29908f
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-04-03 02:30:40 -0400

    playing with the html for the yoga pose details screen in order to use flex display. removed the figure and figcaption

--- src/pages/YogaPose.js
+++ src/pages/YogaPose.js
@@ -11,26 +11,26 @@ class YogaPose extends Component {
           </Link>
         </header>
         <main className="single-pose-details">
-          <figure>
-            <img src={require('../images/yoga_stock_img.jpg')} className="pose-detail-img" alt="tree-pose-image" />
-            <ol className="pose-details-list">
-              <li>Sanskrit Name</li>
-              <li>English Name</li>
-              <li>Position Type</li>
-            </ol>
-            <ol>
-              <h5>Steps to Get into the Pose</h5>
-              <li>Step 1</li>
-              <li>Step 2</li>
-              <li>Step 3</li>
-              <li>Step 4</li>
-              <li>Step 5</li>
-            </ol>
-            <figcaption>
-              <h3>Description</h3>Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird
-              of paradise pose crab pose staff pose flying crow pose.
-            </figcaption>
-          </figure>
+          {/* <figure> */}
+          <img src={require('../images/yoga_stock_img.jpg')} className="pose-detail-img" alt="tree-pose-image" />
+          <ol className="pose-details-list">
+            <li>Sanskrit Name</li>
+            <li>English Name</li>
+            <li>Position Type</li>
+          </ol>
+          <h5>Steps to Get into the Pose</h5>
+          <ol>
+            <li>Step 1</li>
+            <li>Step 2</li>
+            <li>Step 3</li>
+            <li>Step 4</li>
+            <li>Step 5</li>
+          </ol>
+          {/* <figcaption> */}
+          <h3>Description</h3>Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird of
+          paradise pose crab pose staff pose flying crow pose.
+          {/* </figcaption>
+          </figure> */}
         </main>
       </section>
     )
